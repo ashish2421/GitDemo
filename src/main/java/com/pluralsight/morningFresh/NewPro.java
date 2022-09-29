04d1ea1103c3e4242fbb9182f5fdb05381ae6cc6
Ashish <ashishbhosale1911@gmail.com>
2022-09-29 18:11:08 +0530

    The last

---
+++ src/main/java/com/pluralsight/morningFresh/NewPro.java
@@ -0,0 +1,11 @@
+package com.pluralsight.morningFresh;
+
+public class NewPro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Sya");
+	}
+
+}
